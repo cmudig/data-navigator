bacc3791a4490c6873a178e43cd8ee816979b768
Frank Elavsky <frankelavsky@gmail.com>
2023-02-10 13:43:02 -0500

    build off of app file

--- webpack.config.js
+++ webpack.config.js
@@ -1,7 +1,7 @@
 const path = require('path');
 
 module.exports = {
-  entry: './src/interaction-handler.js',
+  entry: './src/app.js',
   output: {
     filename: 'bundle.js',
     path: path.resolve(__dirname, 'dist'),
