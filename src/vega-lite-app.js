6825619d9e68d1b4631df61cd7aa464e3cda2f35
Frank Elavsky <frankelavsky@gmail.com>
2023-03-10 14:39:27 -0500

    add chart and adjust data to vlite app

--- src/vega-lite-app.js
+++ src/vega-lite-app.js
@@ -1,18 +1,51 @@
 import { dataNavigator, describeNode } from './data-navigator';
+import { cars } from './cars.js';
 console.log('yo');
-
+console.log(cars);
+let extents = {
+    Horsepower: [Infinity, -Infinity],
+    Miles_per_Gallon: [Infinity, -Infinity]
+};
+let cleaned = [];
+cars.forEach(x => {
+    const car = { ...x };
+    if (!car['Horsepower']) {
+        console.log('0 horse', car['Horsepower'], car);
+        car['Horsepower'] = 0;
+    }
+    if (!car['Miles_per_Gallon']) {
+        console.log('0 mpg', car['Miles_per_Gallon'], car);
+        car['Miles_per_Gallon'] = 0;
+    }
+    extents['Horsepower'][0] =
+        car['Horsepower'] < extents['Horsepower'][0] ? car['Horsepower'] : extents['Horsepower'][0];
+    extents['Horsepower'][1] =
+        car['Horsepower'] > extents['Horsepower'][1] ? car['Horsepower'] : extents['Horsepower'][1];
+    extents['Miles_per_Gallon'][0] =
+        car['Miles_per_Gallon'] < extents['Miles_per_Gallon'][0]
+            ? car['Miles_per_Gallon']
+            : extents['Miles_per_Gallon'][0];
+    extents['Miles_per_Gallon'][1] =
+        car['Miles_per_Gallon'] > extents['Miles_per_Gallon'][1]
+            ? car['Miles_per_Gallon']
+            : extents['Miles_per_Gallon'][1];
+    cleaned.push(car);
+});
+console.log(extents);
 // vega-lite
-// {
-//     "$schema": "https://vega.github.io/schema/vega-lite/v5.json",
-//     "description": "A scatterplot showing horsepower and miles per gallons for various cars.",
-//     "data": {"url": "data/cars.json"},
-//     "mark": "point",
-//     "encoding": {
-//       "x": {"field": "Horsepower", "type": "quantitative"},
-//       "y": {"field": "Miles_per_Gallon", "type": "quantitative"}
-//     },
-//     "config": {}
-//   }
+const scatter = {
+    $schema: 'https://vega.github.io/schema/vega-lite/v5.json',
+    description: 'A scatterplot showing horsepower and miles per gallons for various cars.',
+    data: cleaned,
+    mark: 'point',
+    encoding: {
+        x: { field: 'Horsepower', type: 'quantitative' },
+        y: { field: 'Miles_per_Gallon', type: 'quantitative' }
+    }
+    // "config": {}
+};
+console.log(scatter);
+vegaEmbed('#root', scatter);
 
 // input data
 let dataUsedInChart = {};
@@ -40,71 +73,71 @@ let dataUsedInChart = {};
 // }
 
 let buildOptions = {
-  data: dataUsedInChart, // required
-  id: 'data-navigator-schema', // required
-  firstNode: 'byj1',
-  rendering: 'on-demand', // "full"
-  manualEventHandling: false, // default is false/undefined
-  root: {
-    cssClass: '',
-    width: '100%',
-    height: 0
-  },
-  navigation: {
-    leftRight: {
-      key: 'series',
-      // flow: "terminal", // could also have circular here (defaults to terminal)
-      rebindKeycodes: {
-        left: 'KeyA',
-        right: 'KeyD'
-      }
+    data: dataUsedInChart, // required
+    id: 'data-navigator-schema', // required
+    firstNode: 'byj1',
+    rendering: 'on-demand', // "full"
+    manualEventHandling: false, // default is false/undefined
+    root: {
+        cssClass: '',
+        width: '100%',
+        height: 0
     },
-    upDown: {
-      key: 'category',
-      // flow: "terminal", // could also have circular here (defaults to terminal)
-      rebindKeycodes: {
-        up: 'KeyW',
-        down: 'KeyS'
-      }
+    navigation: {
+        leftRight: {
+            key: 'series',
+            // flow: "terminal", // could also have circular here (defaults to terminal)
+            rebindKeycodes: {
+                left: 'KeyA',
+                right: 'KeyD'
+            }
+        },
+        upDown: {
+            key: 'category',
+            // flow: "terminal", // could also have circular here (defaults to terminal)
+            rebindKeycodes: {
+                up: 'KeyW',
+                down: 'KeyS'
+            }
+        },
+        backwardForward: {
+            key: 'group',
+            // flow: "terminal", // could also have circular here (defaults to terminal)
+            rebindKeycodes: {
+                forward: 'KeyE',
+                backward: 'KeyQ'
+            }
+        },
+        parentChild: {
+            key: 'level'
+            // flow: "terminal", // could also have circular here (defaults to terminal)
+        }
     },
-    backwardForward: {
-      key: 'group',
-      // flow: "terminal", // could also have circular here (defaults to terminal)
-      rebindKeycodes: {
-        forward: 'KeyE',
-        backward: 'KeyQ'
-      }
-    },
-    parentChild: {
-      key: 'level'
-      // flow: "terminal", // could also have circular here (defaults to terminal)
+    hooks: {
+        navigation: d => {
+            // either a valid keypress is about to trigger navigation (before)
+            // or navigation has just finished
+            // provide another function to interrupt? hmmm...
+            console.log('navigating', d);
+        },
+        focus: d => {
+            // focus has just finished
+            console.log('focus', d);
+        },
+        selection: d => {
+            // selection event has just finished
+            console.log('selection', d);
+        },
+        keydown: d => {
+            // a keydown event has just happened (most expensive hook)
+            console.log('keydown', d);
+        },
+        pointerClick: d => {
+            // the whole nav region has received a click
+            // ideally, we could send the previous focus point and maybe an x/y coord for the mouse?
+            console.log('clicked', d);
+        }
     }
-  },
-  hooks: {
-    navigation: d => {
-      // either a valid keypress is about to trigger navigation (before)
-      // or navigation has just finished
-      // provide another function to interrupt? hmmm...
-      console.log('navigating', d);
-    },
-    focus: d => {
-      // focus has just finished
-      console.log('focus', d);
-    },
-    selection: d => {
-      // selection event has just finished
-      console.log('selection', d);
-    },
-    keydown: d => {
-      // a keydown event has just happened (most expensive hook)
-      console.log('keydown', d);
-    },
-    pointerClick: d => {
-      // the whole nav region has received a click
-      // ideally, we could send the previous focus point and maybe an x/y coord for the mouse?
-      console.log('clicked', d);
-    }
-  }
 };
 
 // create data navigator
@@ -121,42 +154,42 @@ touchHandler.get('pan').set({ enable: false });
 touchHandler.get('swipe').set({ direction: Hammer.DIRECTION_ALL, velocity: 0.2 });
 
 touchHandler.on('press', ev => {
-  // dn.enter()
+    // dn.enter()
 });
 touchHandler.on('pressup', ev => {
-  dn.enter();
+    dn.enter();
 });
 touchHandler.on('swipe', ev => {
-  const larger = Math.abs(ev.deltaX) > Math.abs(ev.deltaY) ? 'X' : 'Y';
-  // const smaller = ev.deltaX <= ev.deltaY ? ev.deltaX : ev.deltaY
-  const ratio =
-    (Math.abs(ev['delta' + larger]) + 0.000000001) /
-    (Math.abs(ev['delta' + (larger === 'X' ? 'Y' : 'X')]) + 0.000000001);
-  const left = ev.deltaX < 0;
-  const right = ev.deltaX > 0;
-  const up = ev.deltaY < 0;
-  const down = ev.deltaY > 0;
-  const direction =
-    ratio > 0.99 && ratio <= 2
-      ? right && up
-        ? 'forward'
-        : right && down
-        ? 'child'
-        : left && down
-        ? 'backward'
-        : left && up
-        ? 'parent'
-        : null
-      : right && larger === 'X'
-      ? 'right'
-      : down && larger === 'Y'
-      ? 'down'
-      : left && larger === 'X'
-      ? 'left'
-      : up && larger === 'Y'
-      ? 'up'
-      : null;
-  if (dn.getCurrentFocus() && direction) {
-    dn.move(direction);
-  }
+    const larger = Math.abs(ev.deltaX) > Math.abs(ev.deltaY) ? 'X' : 'Y';
+    // const smaller = ev.deltaX <= ev.deltaY ? ev.deltaX : ev.deltaY
+    const ratio =
+        (Math.abs(ev['delta' + larger]) + 0.000000001) /
+        (Math.abs(ev['delta' + (larger === 'X' ? 'Y' : 'X')]) + 0.000000001);
+    const left = ev.deltaX < 0;
+    const right = ev.deltaX > 0;
+    const up = ev.deltaY < 0;
+    const down = ev.deltaY > 0;
+    const direction =
+        ratio > 0.99 && ratio <= 2
+            ? right && up
+                ? 'forward'
+                : right && down
+                ? 'child'
+                : left && down
+                ? 'backward'
+                : left && up
+                ? 'parent'
+                : null
+            : right && larger === 'X'
+            ? 'right'
+            : down && larger === 'Y'
+            ? 'down'
+            : left && larger === 'X'
+            ? 'left'
+            : up && larger === 'Y'
+            ? 'up'
+            : null;
+    if (dn.getCurrentFocus() && direction) {
+        dn.move(direction);
+    }
 });
