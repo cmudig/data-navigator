51cce651c45440d8afbdf583882cad350dee51bf
Frank Elavsky <frankelavsky@gmail.com>
2026-02-07 13:51:02 -0500

    ensure we only have one main per page

--- docs/.vitepress/theme/Layout.vue
+++ docs/.vitepress/theme/Layout.vue
@@ -8,7 +8,11 @@ const route = useRoute()
 function ensureMainLandmark() {
   nextTick(() => {
     const vpContent = document.getElementById('VPContent')
-    if (vpContent) {
+    if (!vpContent) return
+    // Only add role="main" if the page doesn't already have a <main> element
+    if (vpContent.querySelector('main')) {
+      vpContent.removeAttribute('role')
+    } else {
       vpContent.setAttribute('role', 'main')
     }
   })
