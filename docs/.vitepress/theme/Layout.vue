5050737eb32fcae9da3c1d274b48d207aebcd5e5
Frank Elavsky <frankelavsky@gmail.com>
2026-02-07 13:31:45 -0500

    wrap content in main landmark

---
+++ docs/.vitepress/theme/Layout.vue
@@ -0,0 +1,23 @@
+<script setup>
+import DefaultTheme from 'vitepress/theme'
+import { onMounted, watch, nextTick } from 'vue'
+import { useRoute } from 'vitepress'
+
+const route = useRoute()
+
+function ensureMainLandmark() {
+  nextTick(() => {
+    const vpContent = document.getElementById('VPContent')
+    if (vpContent) {
+      vpContent.setAttribute('role', 'main')
+    }
+  })
+}
+
+onMounted(ensureMainLandmark)
+watch(() => route.path, ensureMainLandmark)
+</script>
+
+<template>
+  <DefaultTheme.Layout />
+</template>
--- docs/.vitepress/theme/index.ts
+++ docs/.vitepress/theme/index.ts
@@ -1,5 +1,4 @@
 // https://vitepress.dev/guide/custom-theme
-import { h } from 'vue'
 import type { Theme } from 'vitepress'
 import DefaultTheme from 'vitepress/theme'
 import './custom.css'
@@ -7,14 +6,11 @@ import './custom.css'
 // Import our custom components
 import Example from './components/Example.vue'
 import LiveExample from './components/LiveExample.vue'
+import Layout from './Layout.vue'
 
 export default {
   extends: DefaultTheme,
-  Layout: () => {
-    return h(DefaultTheme.Layout, null, {
-      // https://vitepress.dev/guide/extending-default-theme#layout-slots
-    })
-  },
+  Layout,
   enhanceApp({ app, router, siteData }) {
     // Register global components so they can be used in markdown
     app.component('Example', Example)
