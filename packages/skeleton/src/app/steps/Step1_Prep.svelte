ddfc4bc63cfdb5d9ab542fbbdc3239cf5c9a3b41
Frank Elavsky <frankelavsky@gmail.com>
2026-03-22 12:46:41 -0400

    remove structure summary area

--- packages/skeleton/src/app/steps/Step1_Prep.svelte
+++ packages/skeleton/src/app/steps/Step1_Prep.svelte
@@ -2,7 +2,6 @@
     import VariablePanel from '../components/prep/VariablePanel.svelte';
     import DataTablePreview from '../components/prep/DataTablePreview.svelte';
     import QAEngine from '../components/prep/QAEngine.svelte';
-    import StructureSummary from '../components/prep/StructureSummary.svelte';
 </script>
 
 <!-- Step heading receives focus on step transition (see App.svelte focusStepHeading) -->
@@ -18,9 +17,6 @@
     <div class="prep-panel prep-bottom-left">
         <DataTablePreview />
     </div>
-    <div class="prep-panel prep-bottom-right">
-        <StructureSummary />
-    </div>
 </div>
 
 <style>
@@ -28,11 +24,11 @@
         display: grid;
         /* Left column ~35%, right column ~65% */
         grid-template-columns: 35% 65%;
-        /* Top row ~40% of viewport height minus chrome, bottom ~60% */
+        /* Two rows for the left column; right column spans both */
         grid-template-rows: 40vh 55vh;
         grid-template-areas:
             "top-left    top-right"
-            "bottom-left bottom-right";
+            "bottom-left top-right";
         gap: 0;
         /* Escape the workspace-canvas padding so panels bleed to the edges */
         margin: calc(var(--dn-space) * -3);
@@ -41,7 +37,6 @@
     .prep-top-left    { grid-area: top-left; }
     .prep-top-right   { grid-area: top-right; }
     .prep-bottom-left { grid-area: bottom-left; }
-    .prep-bottom-right { grid-area: bottom-right; }
 
     .prep-panel {
         overflow: hidden;
@@ -49,15 +44,14 @@
         border-bottom: 1px solid var(--dn-border);
     }
 
-    /* Remove right border on right-column panels */
-    .prep-top-right,
-    .prep-bottom-right {
+    /* Remove right border on right-column panel */
+    .prep-top-right {
         border-right: none;
     }
 
     /* Remove bottom border on bottom-row panels */
     .prep-bottom-left,
-    .prep-bottom-right {
+    .prep-top-right {
         border-bottom: none;
     }
 </style>
